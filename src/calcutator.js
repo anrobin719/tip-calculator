0011117339cf56f614df518080ac510dbf378158
RobinAn <anrobin719@gmail.com>
2019-05-27 21:38:04 +0900

    add calculator function

--- src/calcutator.js
+++ src/calcutator.js
@@ -1,24 +1,37 @@
-const submit = document.querySelector("#submit");
+function calculateTip() {
+    // Store the data of inputs
+    var billAmount = document.getElementById("bill").value;
+    var serviceQuality = document.getElementById("service").value;
+    var numPeople = document.getElementById("people").value;
 
-function paintTip(text) {
-    const result = document.querySelector("span");
-    result.innerText = text;
+    // Quick validation
+    if(billAmount === "" || serviceQuality == 0) {
+        window.alert("Please enter some values to get this baby up and running!");
+        return;
+    }
+
+    // Check to see if this input is empty of less than or equal to 1
+    if(numPeople === "" || numPeople <= 1) {
+        numPeople = 1;
+        document.getElementById("each").style.display = "none";
+    } else {
+        document.getElementById("each").style.display = "block";
+    }
+
+    // Do some math!
+    var total = (billAmount * serviceQuality) / numPeople;
+    total = Math.round(total * 100) / 100;
+    total = total.toFixed(2);
+
+    document.getElementById("result").style.display = "block";
+    document.getElementById("tip").innerHTML = total;
 }
 
-function handleSubmit(event) {
-    const bill = document.querySelector("#bill").value,
-    service = document.querySelector("#service").value,
-    people = document.querySelector("#people").value;
+document.getElementById("result").style.display = "none";
+document.getElementById("each").style.display = "none";
 
+
+document.getElementById("submit").onclick = function() {
     event.preventDefault();
-    const yourTip = (bill * service) / people;
-
-    console.log(yourTip);
-    paintTip(yourTip);
+    calculateTip();
 }
-
-function init() {
-    submit.addEventListener("submit", handleSubmit);
-}
-
-init();
